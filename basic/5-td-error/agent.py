1b426c0229214fcd02a568ac4bc01d764efd3fc2
Shinichi Yoshinaga <61480734+Shinichi0713@users.noreply.github.com>
2025-05-10 16:27:59 +0900

    up
    Co-Authored-By: Robert-Resmeet <56754319+robert-resnet@users.noreply.github.com>

--- basic/5-td-error/agent.py
+++ basic/5-td-error/agent.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 class Agent:
-    def __init__(self, env):
+    def __init__(self, env, alpha=0.5, gamma=0.9):
         self.status = None
         self.env = env
         
@@ -10,6 +10,8 @@ class Agent:
         self.actions = np.array([-1, 0, 1])
         self.V = np.zeros(len(self.env.states))
         
+        self.alpha = alpha
+        self.gamma = gamma
 
     # td誤差の計算
     def calculate_td_error(self, state):
@@ -20,11 +22,12 @@ class Agent:
             reward, state_next = self.env.get_action(state, action)
             index_state = self.env.return_index_status(state)
             index_state_next = self.env.return_index_status(state_next)
-            td_error += prob_action * (reward + self.V[index_state_next] - self.V[index_state])
+            td_error += prob_action * (reward + self.gamma * self.V[index_state_next] - self.V[index_state])
         return td_error
 
-if __name__ == "__main__":
-    import environment
-    env = environment.Environment()
-    agent_instance = Agent(env=env)
-    print(agent_instance.calculate_td_error('A'))
+    # 価値関数の更新
+    def update_value_function(self, state):
+        td_error = self.calculate_td_error(state)
+        index_state = self.env.return_index_status(state)
+        self.V[index_state] += self.alpha * td_error
+
