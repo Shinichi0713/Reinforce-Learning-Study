6d4d12c7e70bbdb0b9249dae187889c7bd9beaa7
Shinichi Yoshinaga <61480734+Shinichi0713@users.noreply.github.com>
2025-06-25 20:48:24 +0900

    up

---
+++ imitation learning/4-gail/src/demo.py
@@ -0,0 +1,31 @@
+from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.vec_env import DummyVecEnv
+import os
+import gymnasium as gym
+import pickle
+# Create and train an expert model
+env = DummyVecEnv([lambda: gym.make("CartPole-v1")])
+model = PPO("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=10000)
+
+
+import numpy as np
+
+trajectories = []
+obs = env.reset()
+done = False
+
+while not done:
+    action, _ = model.predict(obs)
+    next_obs, reward, done, info = env.step(action)
+    trajectories.append((obs, action, reward, next_obs, done))
+    obs = next_obs
+
+# 保存
+dir_current = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(dir_current, "invader_expert.pickle"), mode="wb") as f:
+    pickle.dump(trajectories, f)
+# np.save(os.path.join(dir_current, 'expert_traj.npy'), trajectories)
+
