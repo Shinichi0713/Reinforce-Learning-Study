e37d9ed155774482ddd21539698700ba963c818b
Shinichi Yoshinaga <yoshishinnze@gmail.com>
2026-05-23 07:29:54 +0900

    up

--- replace.py
+++ replace.py
@@ -64,8 +64,8 @@ def format_to_note_style(content, output_file):
     print(f"変換が完了しました！\n出力先: {output_file}")
 
 # 実行設定
-input_md = r"D:\PycharmProjects\Reinforce-Learning-Study\miulti-agent\petting_zoo\src\2_boxing\doc\sample_dist.md"   # 変換元のファイル名
-output_md = r"D:\PycharmProjects\Reinforce-Learning-Study\miulti-agent\petting_zoo\src\2_boxing\doc\sample_dist.md" # 変換後のファイル名
+input_md = r"D:\PycharmProjects\Reinforce-Learning-Study\basic\doc\0_marcov_chain.md"   # 変換元のファイル名
+output_md = r"D:\PycharmProjects\Reinforce-Learning-Study\basic\doc\0_marcov_chain.md" # 変換後のファイル名
 is_note = False
 
 
